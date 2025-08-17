7183efbd403f32b521e8ee2cc936f51ce41f54be
Kashan-2912 <mkashan2912@gmail.com>
2025-08-17 21:02:20 +0500

    made Messages , Message component, init tailwind-merge and clsx

--- src/app/api/chat-stream/route.ts
+++ src/app/api/chat-stream/route.ts
@@ -1,4 +1,5 @@
 import { ragChat } from "@/lib/rag-chat";
+import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
@@ -6,7 +7,9 @@ export const POST = async (req: NextRequest) => {
 
     const lastMessage = messages[messages.length - 1].content;
 
-    const response = await ragChat.chat(lastMessage);
+    const response = await ragChat.chat(lastMessage, { streaming: true, sessionId });
 
     console.log("RESPONSE:", response);
+
+    return aiUseChatAdapter(response);
 }
